e5ab9f0c3f811fd6cd22f289f1d95c1ee2607dc8
Urban Eriksson <123ube@gmail.com>
2020-04-12 12:33:52 +0200

    Bug fixes plus streamlining the repo

--- gaussifier.py
+++ gaussifier.py
@@ -3,54 +3,61 @@ import pandas as pd
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-
 class Gaussifier(object):
-    def __init__(self):
-        self._x = None
-        self._delta = None
-        self._xmin = None
-        self._xmax = None
     
     def fit(self,training_data):
         N = len(training_data)
         self._delta = 1 / (N + 1)
         self._x = np.sort(training_data)
         self._xmin = self._x[0]
+        self._xmin_transformed = norm.ppf(1 / (N + 1))
         self._xmax = self._x[-1]
+        self._xmax_transformed = norm.ppf(N / (N + 1))
+        self._sigma = np.std(training_data)
 
-    def execute(self, production_data):
-        output = np.zeros_like(production_data)
-        for i,val in zip(range(len(production_data)),production_data):
-            if val > self._xmin and val < self._xmax:
-                ix1 = np.where( val < self._x) [0][0]
-                ix2 = np.where( val <= self._x) [0][0]
-                pp = self._delta * 0.5 * (1 + ix1 + ix2)
-            elif val < self._xmin:
-                pp = self._delta / 2
+    def execute(self, test_data):
+        output = np.zeros_like(test_data)
+        for i,val in zip(range(len(test_data)),test_data):
+            if val > self._xmin and val <= self._xmax:
+                ix = np.where( val <= self._x) [0][0] - 1
+                dx =  self._x[ix + 1] - self._x[ix]
+                k = (val - self._x[ix]) / dx
+                pp = (1 + ix + k) / (N + 1)
+                output[i] = norm.ppf(pp)
+            elif val <= self._xmin:
+                x_tail = (val - self._xmin) / self._sigma
+                output[i] = self._xmin_transformed + x_tail
             elif val > self._xmax:
-                pp = 1 - self._delta / 2
-            elif val == self._xmin:
-                pp = self._delta
-            elif val == self._xmax:
-                pp = 1 - self._delta
-            output[i] = norm.ppf(pp)
+                x_tail = (val - self._xmax) / self._sigma
+                output[i] = self._xmax_transformed + x_tail
         return output
 
-
 N = 1000
-test_data = np.linspace(1, N, N)
+
+train_data = np.random.uniform(0, 100, N)
 g = Gaussifier()
-g.fit(test_data)
-output = g.execute(test_data)
+g.fit(train_data)
 
-print('Mean: ' + str(np.mean(output)))
-print('Std: ' + str(np.std(output)))
-
+output = g.execute(train_data)
+print('Mean (test==train): ' + str(np.mean(output)))
+print('Std (test==train): ' + str(np.std(output)))
 fig = plt.figure(figsize = (8,6))
 ax = fig.gca()
 pd.DataFrame(output).hist(ax=ax,color='#27aaff',edgecolor='#666666', linewidth=1.0)
 plt.xlabel('Value')
 plt.ylabel('Count')
-plt.title('Test and production data the same')
+plt.title('Train and test data are identical')
 plt.show()
 
+test_data = np.random.uniform(0, 100, N)
+
+output = g.execute(test_data)
+print('Mean (test!=train): ' + str(np.mean(output)))
+print('Std (test!=train): ' + str(np.std(output)))
+fig = plt.figure(figsize = (8,6))
+ax = fig.gca()
+pd.DataFrame(output).hist(ax=ax,color='#b90084',edgecolor='#666666', linewidth=1.0)
+plt.xlabel('Value')
+plt.ylabel('Count')
+plt.title('Train and test data are different samples from the same uniform distributtion')
+plt.show()
